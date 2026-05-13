e5d567b9399fdc6d7cd8d2f79dede1fb83ca9cad
mattjgriffiths <mattjgriffiths@gmail.com>
2026-05-13 13:34:37 +0100

    Delete MultiResourceModules directory

--- MultiResourceModules/ResourceGroup/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "resourcegroup" {
-  name     = var.rg_name
-  location = var.location
-  tags       = {
-    Environment   = var.environment
-    Department    = var.department
-    Owner         = var.owner
-  }
-}
--- MultiResourceModules/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "RGName" {
-  value = module.ResourceGroup.rg_name_out
-}
-
-#output "KVName" {
-#  value = module.KeyVault.kv_name_out
-#}
